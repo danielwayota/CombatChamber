849df8a87274e059afb74c6916b1bcfaa2604733
Daniel Wayota <danielglez1990@gmail.com>
2020-07-13 11:59:32 +0100

    Project Base (updated)

--- Assets/Scripts/Player/PlatformPlayer.cs
+++ Assets/Scripts/Player/PlatformPlayer.cs
@@ -8,6 +8,8 @@ public class PlatformPlayer : MonoBehaviour
 
     private Rigidbody2D body;
 
+    private bool lookingRight;
+
     /// =============================================
     /// <summary>
     ///
@@ -15,6 +17,8 @@ public class PlatformPlayer : MonoBehaviour
     void Start()
     {
         this.body = this.GetComponent<Rigidbody2D>();
+
+        this.lookingRight = true;
     }
 
     /// =============================================
@@ -23,16 +27,41 @@ public class PlatformPlayer : MonoBehaviour
     /// </summary>
     void Update()
     {
-        var movement = this.body.velocity;
-
         float h = Input.GetAxisRaw("Horizontal");
-        movement.x = h * this.speed;
 
+        this.FlipSprite(h);
+
+        var movement = this.body.velocity;
+        movement.x = h * this.speed;
         if ((movement.y == 0) && Input.GetButtonDown("Jump"))
         {
             movement.y = this.jumpPower;
         }
 
         this.body.velocity = movement;
+    }
+
+    /// =============================================
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="direction"></param>
+    void FlipSprite(float direction)
+    {
+        if (direction == 0)
+            return;
+
+        // Got to right
+        if (direction > 0 && this.lookingRight == false)
+        {
+            this.lookingRight = true;
+            this.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        // Got to left
+        else if (direction < 0 && this.lookingRight == true)
+        {
+            this.lookingRight = false;
+            this.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
     }
 }
